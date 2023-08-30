4effa18e273b70b6e690a82dc9a3d309e6883a73
Rylan Schaeffer <rylanschaeffer@meta.com>
2023-08-30 11:46:28 -0700

    Plotted Dataset Cartography

--- papers/dataset_cartography_swayamdipta_2020/dataset_cartography_swayamdipta_2020.py
+++ papers/dataset_cartography_swayamdipta_2020/dataset_cartography_swayamdipta_2020.py
@@ -23,35 +23,50 @@ table_2_rows = [
 datasets = ["WinoG Val (ID)", "WSC (OOD)"]
 statistics = ["Mean", "Std Dev", "Num Samples"]
 
-table_2_cols = pd.MultiIndex.from_product([datasets, statistics], names=["first", "second"])
+table_2_cols = pd.MultiIndex.from_product(
+    [datasets, statistics], names=["first", "second"]
+)
 
 
-table_2_data = np.array([
-    [79.7, 0.2, 3, 86.0, 0.1, 3],
-    [73.3, 1.3, 3, 85.6, 0.4, 3],
-    [70.8, 0.6, 3, 84.1, 0.4, 3],
-    [69.4, 0.5, 3, 83.9, 0.5, 3],
-    [70.1, 1.0, 3, 83.7, 1.4, 3],
-    [78.2, 0.6, 3, 86.3, 0.6, 3],
-    [77.9, 1.3, 3, 87.2, 0.7, 3],
-    [78.7, 0.4, 3, 87.6, 0.6, 3],
-])
+table_2_data = np.array(
+    [
+        [79.7, 0.2, 3, 86.0, 0.1, 3],
+        [73.3, 1.3, 3, 85.6, 0.4, 3],
+        [70.8, 0.6, 3, 84.1, 0.4, 3],
+        [69.4, 0.5, 3, 83.9, 0.5, 3],
+        [70.1, 1.0, 3, 83.7, 1.4, 3],
+        [78.2, 0.6, 3, 86.3, 0.6, 3],
+        [77.9, 1.3, 3, 87.2, 0.7, 3],
+        [78.7, 0.4, 3, 87.6, 0.6, 3],
+    ]
+)
 
 table_2_df = pd.DataFrame(table_2_data)
 table_2_df.index = table_2_rows
 table_2_df.columns = table_2_cols
 for dataset in datasets:
+    table_2_df[dataset, "Std Err"] = table_2_df[dataset]["Std Dev"] / np.sqrt(
+        table_2_df[dataset]["Num Samples"]
+    )
+
+
+for dataset in datasets:
     table_2_dataset_df = table_2_df[dataset]
     plt.close()
     y = np.arange(len(table_2_dataset_df))[::-1]
     x = table_2_dataset_df["Mean"]
-    xerr = table_2_dataset_df["Std Dev"] / np.sqrt(table_2_dataset_df["Num Samples"])
-    plt.errorbar(x, y, xerr=xerr, linestyle='None', marker='^')
+    xerr = table_2_dataset_df["Std Err"]
+    plt.errorbar(x, y, xerr=xerr, linestyle="None", marker="^")
     # Add y tick labels based on table_2_dataset_df.index
     plt.yticks(y, table_2_dataset_df.index.values)
+    plt.xlabel("Accuracy")
+    plt.title(
+        "Fig 2 from Swayamdipta et al.'s Dataset Cartography (2020)\nDataset: "
+        + dataset
+    )
     save_plot_with_multiple_extensions(
         plot_dir=paper_dir,
-        plot_title=f"table_2_dataset={dataset.lower().replace(' ', '_')}"
+        plot_title=f"table_2_dataset={dataset.lower().replace(' ', '_')}",
     )
     plt.show()
 
